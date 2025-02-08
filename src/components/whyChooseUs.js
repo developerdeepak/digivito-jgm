3427e66c5a8806036b315c582290637405c52575
Deepak Garg <softwaredeveloper94@gmail.com>
2025-02-08 18:20:16 +0530

    UI: why choose us section on home page

--- src/components/whyChooseUs.js
+++ src/components/whyChooseUs.js
@@ -35,7 +35,7 @@ const BannerDisclaimer = (props) => {
 
 const BannerCTA = (props) => {
     return (
-        <Link to={props.cta.link} className="btn cta btn-outline-white rounded-pill poppins-bold">
+        <Link to={props.cta.link} className="btn cta btn-primary text-white rounded-pill poppins-bold">
             {HTMLParser.parseHTML(props.cta.text)}
         </Link>
     );
@@ -66,13 +66,13 @@ const WhyChooseUs = (props) => {
 
     return (
         <>
-            <div className={"row banner-side-by-side align-items-center justify-content-center " + (props.bannerBgClass ? props.bannerBgClass : "bg-primary")}>
+            <div className={"row banner-side-by-side why-choose-banner align-items-center justify-content-center " + (props.bannerBgClass ? props.bannerBgClass : "bg-primary")}>
                 <div className="col">
                     <div className={"row align-items-center " + (props.isBannerRight ? "justify-content-end" : "")}>
-                        <div className={"col-12 col-lg-6 banner-container d-none d-lg-block " + (props.isBannerRight ? "pe-0" : "ps-0")}>
-                            <img src={props.BannerImage} className="mw-100" alt="Ready to consult banner" />
+                        <div className={"col-12 banner-container d-none d-lg-block " + (props.isBannerRight ? "pe-0" : "ps-0")}>
+                            <img src={props.BannerImage} className="w-100 desktop-banner" alt="Ready to consult banner" />
                         </div>
-                        <div className="d-lg-none col-12 col-lg-6 d-flex align-items-center py-5 py-lg-0 text-container">
+                        <div className="d-lg-none col-12 d-flex align-items-center py-5 py-lg-0 text-container">
                             <div className="content d-flex flex-column">
                                 <div className="text-content d-flex flex-column">
                                     {bannerSubHeading}
@@ -80,15 +80,19 @@ const WhyChooseUs = (props) => {
                                     {bannerDesc}
                                     {bannerDisclaimer}
                                 </div>
-                                <img src={props.BannerImage} className="mw-100 mobile-banner" alt="Ready to consult banner" />
+                                <div className="row">
+                                    <div className="col ps-0">
+                                        <img src={props.BannerImage} className="w-100 mobile-banner" alt="Ready to consult banner" />
+                                    </div>
+                                </div>
                                 {bannerCTA}
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col mw-1600 mx-auto d-none d-lg-block position-absolute">
+                <div className="col mw-1600 mx-auto d-none d-lg-block">
                     <div className={"row align-items-center " + (!props.isBannerRight ? "justify-content-end" : "")}>
-                        <div className="col-12 col-lg-6 d-flex align-items-center py-5 py-lg-0 text-container">
+                        <div className="col-12 d-flex align-items-center py-5 py-lg-0 text-container">
                             <div className="content d-flex flex-column">
                                 <div className="text-content d-flex flex-column">
                                     {bannerSubHeading}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import Components from "../components";
 import AppData from '../data/data.json';
 import HeroBannerImage from '../images/home-hero-banner-lg.jpg';
 import ScheduleFreeConsultationImage from '../images/schedule-free-consultation.jpg';
+import LawHammerBanner from '../images/law-hammer-banner.png';
 
 const pageSpecificData = AppData.pages.home;
 const getInTouchData = {
@@ -19,7 +20,7 @@ const Home = () => {
             <Components.TwoColumnList {...pageSpecificData.practiceAreas} />
             <Components.BannerSideBySide {...pageSpecificData.scheduleFreeConsultation} BannerImage={ScheduleFreeConsultationImage} />
             <Components.QuoteCEO />
-            <Components.WhyChooseUs {...pageSpecificData.whyChooseUs} BannerImage={ScheduleFreeConsultationImage} />
+            <Components.WhyChooseUs {...pageSpecificData.whyChooseUs} BannerImage={LawHammerBanner} />
             <Components.Testimonials {...pageSpecificData.testimonials} />
             <Components.GetInTouch {...getInTouchData} />
         </>
